2a60065ad112aba5014be248e75148484a68ba2e
S_Shomo <90357187+SShomo@users.noreply.github.com>
2024-01-25 13:51:52 -0500

    Unity + Tiles

---
+++ Wave Function Collapse/Assets/Scripts/WaveFunctionCollapse.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaveFunctionCollapse : MonoBehaviour
+{
+
+    public Tile[] tiles;
+    public Tilemap grid;
+
+    public void Preload()
+    {
+
+    }
+}
